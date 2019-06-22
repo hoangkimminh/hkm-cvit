ac20323d585c246b0e3c2244a1b87830bfe83987
hoangkimminh <vatliyeutoi@gmail.com>
2019-06-22 09:52:03 +0700

    Init project hkm-cvit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  redner() {
+    (
+      <div className="App">
+        <header className="App-header">
+          <nav class="nav nav-tabs nav-stacked">
+            <a class="nav-link active" href="#">Active link</a>
+            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link disabled" href="#">Disabled link</a>
+          </nav>
+        </header>
+
+      </div>
+    );
+  }
 }
 
 export default App;
