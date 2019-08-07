8c42660d199f82fa7ad019edfa739fd1c6191920
hoangkimminh <vatliyeutoi@gmail.com>
2019-08-07 09:05:33 +0700

    Build for deploy

--- src/personal-info/profile.js
+++ src/personal-info/profile.js
@@ -3,7 +3,7 @@ export default {
   DOB: "3th March, 1999",
   phoneNumber: "+84344955825",
   email: "minhhk0312.uet@gmail.com",
-  address: "Hiep 1 village, Minh Khai commune, Hoai Duc district, Hanoi city",
+  address: "Minh Hiep 1 village, Minh Khai commune, Hoai Duc district, Hanoi city",
   school:
     "Student at University of Engineering and Technology, Vietnam National University (VNU)",
   character: "Sociable, hardworking, studious, enthusiastic",
